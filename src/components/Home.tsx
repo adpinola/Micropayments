c78d9684c10a7b2824df021827b67882c6999d26
Alejandro Pinola <alejandro.pinola@southworks.com>
2022-02-12 21:42:04 -0300

    homepage styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Col, Container, Image, Nav, Navbar, Row } from 'react-bootstrap';
 import '../styles/Home.scss';
+import Logo from '../assets/ethereum-logo.svg';
 
-const Home: FC = () => {
-  return (
-    <Navbar bg="light" expand="lg">
+const Home: FC = () => (
+  <div id="home">
+    <Navbar bg="primary" expand="lg" className="navbar" fixed="top">
       <Container>
         <Navbar.Brand href="/">Micropayments Factory</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
@@ -16,7 +17,20 @@ const Home: FC = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
-};
+    <Container className="content">
+      <Row>
+        <Col>
+          <div>
+            <h2>Manage payments using this integrated platform.</h2>
+            <h4>Just create contracts and enable payments by delivering a signed message. Easy for you as employer or employee.</h4>
+          </div>
+        </Col>
+        <Col>
+          <Image width="150" src={Logo} />
+        </Col>
+      </Row>
+    </Container>
+  </div>
+);
 
 export default Home;
