c79f4b60d3ffff4fa7102c89c2d3b6d9cc2b86a8
Alejandro Pinola <alejandro.pinola@southworks.com>
2022-02-14 23:51:59 -0300

    add form names, transition effects and states

--- src/components/Contractor.tsx
+++ src/components/Contractor.tsx
@@ -1,11 +1,34 @@
 import React, { FC, useState } from 'react';
 import { Button, Col, Container, Row, Card, Modal, FormControl, InputGroup, FloatingLabel } from 'react-bootstrap';
 import '../styles/Contractor.scss';
+import MetaMaskIcon from './MetaMaskIcon';
+
+enum WalletStatus {
+  Locked = 'Locked',
+  Connected = 'Connected',
+}
 
 const Contractor: FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [balance, setBalance] = useState<number>(0);
+  const [walletStatus, setWalletStatus] = useState<WalletStatus>(WalletStatus.Locked);
   const [showModal, setShowModal] = useState(false);
+  const [claimValues, setClaimValues] = useState({});
+
+  const claimPayment = () => {
+    console.log(claimValues);
+    setShowModal(false);
+  };
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setClaimValues((values) => ({ ...values, [name]: value }));
+    // ToDo: Perform field validations
+  };
+
+  const connectWallet = () => {
+    setWalletStatus(WalletStatus.Connected);
+  };
 
   return (
     <div id="contractor">
@@ -15,10 +38,17 @@ const Contractor: FC = () => {
             <Card className="text-center" border="dark" bg="light">
               <Card.Header>Your account&apos;s balance</Card.Header>
               <Card.Title as="h1">{balance} ETH</Card.Title>
-              <Card.Body>
-                <Button variant="secondary" onClick={() => setShowModal(true)}>
-                  Claim
-                </Button>
+              <Card.Body className="d-flex justify-content-center">
+                {walletStatus === WalletStatus.Connected ? (
+                  <Button variant="secondary" onClick={() => setShowModal(true)}>
+                    Claim a payment
+                  </Button>
+                ) : (
+                  <Button variant="secondary" className="d-flex" onClick={connectWallet}>
+                    <MetaMaskIcon />
+                    <div>Connect to MetaMask</div>
+                  </Button>
+                )}
               </Card.Body>
             </Card>
           </Col>
@@ -30,21 +60,27 @@ const Contractor: FC = () => {
         </Modal.Header>
         <Modal.Body>
           <FloatingLabel label="Contract Address" className="mb-3">
-            <FormControl aria-label="target contract address" placeholder="0x..." />
+            <FormControl name="address" placeholder="0x..." onChange={handleChange} required />
           </FloatingLabel>
           <InputGroup className="mb-3">
-            <FormControl aria-label="ETH amount to claim" placeholder="0.0000" />
+            <FormControl name="amount" placeholder="0.0000" onChange={handleChange} />
             <InputGroup.Text>ETH</InputGroup.Text>
           </InputGroup>
           <InputGroup className="mb-3">
-            <FormControl aria-label="nonce" placeholder={Date.now().toString()} />
+            <FormControl name="nonce" placeholder={Date.now().toString()} onChange={handleChange} />
           </InputGroup>
           <FloatingLabel label="Signature" className="mb-3">
-            <FormControl style={{ minHeight: '150px', maxHeight: '250px' }} as="textarea" aria-label="signature" placeholder="0x..." />
+            <FormControl
+              style={{ minHeight: '150px', maxHeight: '250px' }}
+              as="textarea"
+              name="signature"
+              placeholder="0x..."
+              onChange={handleChange}
+            />
           </FloatingLabel>
         </Modal.Body>
         <Modal.Footer>
-          <Button>Confirm</Button>
+          <Button onClick={claimPayment}>Confirm</Button>
         </Modal.Footer>
       </Modal>
     </div>
